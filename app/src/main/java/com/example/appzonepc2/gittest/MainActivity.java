83b74ea442d585f7024e525930c3df877d02ffea
Appzone PC 2 <appzonepc2@Appzones-MacBook-Pro.local>
2018-01-10 20:44:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/appzonepc2/gittest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.appzonepc2.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void testupload(){
+        int i = 1+1;
+    }
+}
